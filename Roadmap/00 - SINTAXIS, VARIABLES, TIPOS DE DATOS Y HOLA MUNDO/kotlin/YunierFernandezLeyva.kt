6a1978210c7215b35bbc3c73ffe07e28a93b5ef2
Yunier Fernández <yunierfdezl@gmail.com>
2024-01-23 21:41:09 +0100

    Create YunierFernandezLeyva.kt

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/YunierFernandezLeyva.kt
@@ -0,0 +1,36 @@
+// Kotlin: https://kotlinlang.org/
+
+
+//Sintaxis para crear comentarios en una línea
+
+/*
+Sintaxis para crear comentarios
+en varias líneas
+ */
+ const val idioma= "Español"
+
+ fun main(){
+var pais = "España" 
+
+//Variables de datos primitivos
+val entero: Int = 10
+val enteroLargo: Long = 10022011L
+val enteroCorto: Short = 1
+val byte: Byte = 25
+val doble: Double = 2.2
+val flotante: Float = 3.3f
+val caracter: Char = 'a'
+val boleano: Boolean = true
+
+
+//Cadenas de texto
+val cadena: String = "¡Hola. Kotlin!"
+
+println(cadena)
+
+}
+
+
+
+
+ 
